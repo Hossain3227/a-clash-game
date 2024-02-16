c439e58a7311208a0ee55e94aca2e98a762b874a
Hossain3227 <hasan32.hk27@gmail.com>
2024-02-16 15:51:46 +0600

    score added

--- scripts/al-clash.js
+++ scripts/al-clash.js
@@ -25,6 +25,30 @@ function setBgById(elementid){
  const element = document.getElementById(elementid);
  element.classList.add('bg-orange-500');
 }
+function removeBgById(elementid){
+ const element = document.getElementById(elementid);
+ element.classList.remove('bg-orange-500');
+}
+
+function getTextElementById(elementid){
+    const element = document.getElementById(elementid);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+
+}
+
+function setTextElementValueById(elementid, value){
+    const element = document.getElementById(elementid);
+    element.innerText = value;
+}
+
+
+function getElementTextId(elementid){
+    const element = document.getElementById(elementid);
+    const text = element.innerText;
+    return text;
+}
 
 
 function play(){
@@ -34,9 +58,89 @@ function play(){
     // console.log(homeSection.classList);
     
     hideElementById('home');
+    hideElementById('final-score');
     showElementById('playground');
+
+
+    // rest life and score
+
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
     continueGame();
 }
+
+
+function handleKeyBoard(event){
+    const playerPressed = event.key;
+    console.log(playerPressed);
+
+    //if player pressed esc stop the game
+     if(playerPressed === 'Escape'){
+        gameOver();
+     }
+
+    const currentAlpha = document.getElementById('current-alphabet');
+    const current = currentAlpha.innerText;
+    const expectedAlpha = current.toLowerCase();
+    console.log(playerPressed,expectedAlpha);
+
+    //check matched or not 
+
+    if(playerPressed === expectedAlpha){
+        console.log('you get a point');
+        //update score
+            const currentScore = getTextElementById('current-score');
+            console.log(currentScore);
+            const updatesScore = currentScore + 1;
+            setTextElementValueById('current-score', updatesScore);
+            
+
+
+
+        // const currentScoreEle = document.getElementById('current-score'); 
+        // const currentscText = currentScoreEle.innerText;
+        // const currentSc = parseInt(currentscText);
+
+        // console.log(currentscText);
+
+
+        //increase score by 1
+
+        // const newScore = currentSc + 1;
+
+        //update
+        // currentScoreEle.innerText = newScore;
+        //start a new game
+        removeBgById(expectedAlpha);
+        continueGame();
+    }
+    else {
+        console.log('you missed, you lost a life');
+
+        const currentLife = getTextElementById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        if(updatedLife === 0){
+            gameOver();
+
+        }
+
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        //update life 
+
+        // const newLife = currentLife -1;
+
+        // currentLifeElement.innerText = newLife;
+
+    }
+}
+
+document.addEventListener('keyup', handleKeyBoard);
+
 
 function continueGame(){
   const alphabet = getArandomAlphabet();
@@ -46,3 +150,18 @@ function continueGame(){
   setBgById(alphabet);
 }
 
+
+function gameOver(){
+    hideElementById('playground');
+    showElementById('final-score');
+
+    //update final score 
+
+    const lastScore = getTextElementById('current-score');
+    setTextElementValueById('game-score',lastScore);
+
+    //clear the last selected alphabet highlight 
+    const currentAlphabet = getElementTextId('current-alphabet');
+    removeBgById(currentAlphabet);
+
+}
